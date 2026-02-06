a87ca4606ebc55c2047526f021d0f00619237b06
Eldana_M <moldobayevae@gmail.com>
2026-02-06 21:33:58 +0500

    Improved create view with GET method and better validation

--- f1_teams/views.py
+++ f1_teams/views.py
@@ -5,11 +5,22 @@ from .models import F1Team
 from .serializers import F1TeamSerializer
 
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])  
 def create_f1_team(request):
+    if request.method == 'GET':
+        teams = F1Team.objects.all()
+        serializer = F1TeamSerializer(teams, many=True)
+        return Response(serializer.data)
+    
     if request.method == 'POST':
-        serializer = F1TeamSerializer(data=request.data)
+        data = request.data.copy()
+        if 'championships_won' in data and int(data['championships_won']) < 0:
+            return Response({"error": "Чемпионств не может быть меньше 0!"}, 
+                          status=400)
+        
+        serializer = F1TeamSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"message": "Команда создана!", "data": serializer.data}, 
+                          status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
